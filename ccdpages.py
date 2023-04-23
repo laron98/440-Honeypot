056c79827ed83c8b6d56012c95d965380b8ab670
laron98 <72889951+laron98@users.noreply.github.com>
2023-04-22 21:19:53 -0700

    Update ccdpages.py

--- ccdpages.py
+++ ccdpages.py
@@ -1,7 +1,15 @@
-from flask import Flask, redirect, render_template, request, url_for 
+from flask import Flask, redirect, render_template, request, url_for, send_from_directory 
 from flask_sqlalchemy import SQLAlchemy
 from markupsafe import escape
+import os
 
+class user:
+    def __init__(self, fname, lname, email, pword, db):
+        self.fname = fname
+        self.lname = lname
+        self.email = email
+        self.pword = pword
+        self.db = db
 
 app = Flask(__name__)
 
@@ -10,38 +18,49 @@ app = Flask(__name__)
 #Home page
 @app.route('/')
 def index():
-    return render_template('homepage.html')
+    image_url = 'static\images\cclogo.png'
+    return render_template('homepage.html', image_url=image_url)
 
 #sign up page
 @app.route('/signup')
 def sign_up():
-    return render_template('signup.html')
+    image_url = 'static\images\cclogo.png'
+    return render_template('signup.html', image_url=image_url)
 
 #checks submitted data from users and if valid puts on database
 @app.route('/signup/verify', methods=['GET','POST'])
 def submit():
-    fname = request.form['fname']
-    lname = request.form['lname']
-    email = request.form['email']
+    newuser = user('', '', '', '', '')
+    newuser.fname = request.form['fname']
+    newuser.lname = request.form['lname']
+    newuser.email = request.form['email']
     password = request.form['password']
     cpassword = request.form['cpassword']
     
     if (password == cpassword):
         #To do: enter into data base
+        newuser.pword = password
         return redirect(url_for('signup/fail'))
 
     #if passwords don't match have user try again 
     return redirect(url_for('/submit/success'))
 
 #If user enters incorrect data they are redirected to this page
-@app.route('/submit/fail')
+@app.route('/signup/fail')
 def sign_up_fail():
-    return render_template('signupfail.html')
+    image_url = 'static\images\cclogo.png'
+    return render_template('signupfail.html', image_url=image_url)
+
+@app.route('/signup/success')
+def sign_up_success():
+    image_url = 'static\images\cclogo.png'
+    return render_template('signupsuccess.html', image_url=image_url)
 
 #User login page
 @app.route('/login')
 def login():
-    return  render_template('login.html')
+    image_url = 'static\images\cclogo.png'
+    return  render_template('login.html', image_url=image_url)
 
 @app.route('/login/verify', methods=['GET', 'POST'])
 def login_verf():
@@ -53,14 +72,18 @@ def login_verf():
         #verify credentials with database
         #if valid login
         if():
-            return redirect(url_for('/userhome'))
+            return redirect(url_for('/home'))
         else: 
             return redirect(url_for('login/fail'))
     else: 
         return redirect(url_for('/signup/verify'))
     
+@app.route('/home')
+def home():
+    image_url = 'static\images\cclogo.png'
+    #put data from database to into user class 
+    #userdb = 
+    return render_template('home.html', image_url=image_url) #, userdb=userdb) 
 
 if __name__ == '__main__':
     app.run()
-
-
