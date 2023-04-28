c6eb5d1adfe9cb5baf0efa1ff01d759f6517197e
BrianMcMurr <61162591+BrianMcMurr@users.noreply.github.com>
2023-04-27 21:35:13 -0700

    Update Audit_Alert_Emailer.py
    IT WORKS ITS ALIVE

--- Audit_Alert_Emailer.py
+++ Audit_Alert_Emailer.py
@@ -1,12 +1,11 @@
 import time
-import smtplib
+import requests
 from email.mime.text import MIMEText
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 # Replace these variables with your own email and password
-EMAIL_ADDRESS = 'Insert_Email_Here'
-EMAIL_PASSWORD = 'Insert_Email_Password_Here'
+API_KEY = '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'
 
 # Define the path of the file you want to monitor
 FILE_PATH = '/var/log/audit/audit.log'
@@ -16,20 +15,42 @@ message = 'The file at {} has been modified.'.format(FILE_PATH)
 
 # Define the email function
 def send_email():
-    msg = MIMEText(message)
-    msg['Subject'] = 'File Modification Alert'
-    msg['From'] = EMAIL_ADDRESS
-    msg['To'] = EMAIL_ADDRESS
+    url = "https://api.sendinblue.com/v3/smtp/email"
 
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-        smtp.send_message(msg)
+    payload = {
+        "sender": {
+            "name": "HoneyPotAlerter",
+            "email": "auditemail440@gmail.com"
+        },
+        "to": [
+            {
+                "email": "auditemail440@gmail.com",
+                "name": "HoneyPotAdmin"
+            }
+        ],
+        "htmlContent": "<html><body>{}</body></html>".format(message),
+        "subject": "File Modification Alert"
+    }
+
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "api-key": API_KEY
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+
+    if response.status_code == 201:
+        print("Email sent successfully.")
+    else:
+        print("Error sending email. Status code:", response.status_code)
 
 # Define the event handler
 class FileModifiedEventHandler(FileSystemEventHandler):
     def on_modified(self, event):
         if event.src_path == FILE_PATH:
             send_email()
+
 # Start the observer
 if __name__ == '__main__':
     event_handler = FileModifiedEventHandler()
